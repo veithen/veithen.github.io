5da85dc6495e2ff3e7ff5164ed7b034cc8cc0dad
Andreas Veithen <andreas.veithen@gmail.com>
2014-11-23 15:38:51 +0000

    Use tabs to navigate between tech and opinion posts

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,41 @@
 (function(){
     $(document).ready(function() {
-        var tags = {}
-        $("ul.post-list meta[itemprop='keywords']").each(function() {
-            $.each($(this).attr("content").split(","), function(index, tag) {
-                if (tags[tag]) {
-                    tags[tag]++
-                } else {
-                    tags[tag] = 1
-                }
-            })
-        })
-        $.each(tags, function(tag, count) {
-            var li = $("<li></li>")
-            li.appendTo($("ul.tag-box"))
-            var a = $("<a href=''>" + tag + " <span>" + count + "</span></a>")
-            a.appendTo(li)
-            a.click(function(e) {
-                e.preventDefault()
-                $("ul.post-list meta[itemprop='keywords']").each(function() {
-                    var li = $(this).parent().parent()
-                    var matches = $(this).attr("content").split(",").indexOf(tag) != -1
-                    if (matches != $(li).is(":visible")) {
-                        if (matches) {
-                            $(li).show("slow")
-                        } else {
-                            $(li).hide("slow")
-                        }
+        $("#tabs").tabs();
+        $(".ui-tabs-panel").each(function() {
+            var tagCloud = $(this).find("ul.tag-box")
+            var postList = $(this).find("ul.post-list")
+            var tags = {}
+            postList.find("meta[itemprop='keywords']").each(function() {
+                $.each($(this).attr("content").split(","), function(index, tag) {
+                    if (tags[tag]) {
+                        tags[tag]++
+                    } else {
+                        tags[tag] = 1
                     }
                 })
             })
+            $.each(tags, function(tag, count) {
+                var li = $("<li></li>")
+                li.appendTo(tagCloud)
+                var a = $("<a href=''>" + tag + " <span>" + count + "</span></a>")
+                a.appendTo(li)
+                a.click(function(e) {
+                    e.preventDefault()
+                    postList.find("meta[itemprop='keywords']").each(function() {
+                        var li = $(this).parent().parent()
+                        var matches = $(this).attr("content").split(",").indexOf(tag) != -1
+                        if (matches != $(li).is(":visible")) {
+                            if (matches) {
+                                $(li).show("slow")
+                            } else {
+                                $(li).hide("slow")
+                            }
+                        }
+                    })
+                })
+            })
+            postList.find("li").hide();
+            postList.find("li").removeClass("hidden");
         })
-        $("ul.post-list li").hide();
-        $("ul.post-list li").removeClass("hidden");
     });
 })()
