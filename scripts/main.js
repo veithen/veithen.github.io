a93b0ff006bc12bede792a240080fb15607e288f
Andreas Veithen <andreas.veithen@gmail.com>
2014-11-23 12:28:20 +0000

    Generate tag cloud dynamically with jQuery

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
 (function(){
     $(document).ready(function() {
-        $("ul.tag-box li a").click(function(e){
-            e.preventDefault()
-            $("ul.post-list li").hide();
-            $("ul.post-list li").removeClass("hidden");
-            $("ul.post-list li article[data-tag-"+$(this).attr("data-tag")+"]").parent().fadeIn()
-        })
+    	var tags = {}
+    	$("ul.post-list meta[itemprop='keywords']").each(function() {
+    		$.each($(this).attr("content").split(","), function(index, tag) {
+    			if (tags[tag]) {
+    				tags[tag]++
+    			} else {
+    				tags[tag] = 1
+    			}
+    		})
+    	})
+    	$.each(tags, function(tag, count) {
+    		var li = $("<li></li>")
+    		li.appendTo($("ul.tag-box"))
+    		var a = $("<a href=''>" + tag + " <span>" + count + "</span></a>")
+    		a.appendTo(li)
+            a.click(function(e) {
+                e.preventDefault()
+                $("ul.post-list li").hide();
+                $("ul.post-list li").removeClass("hidden");
+                $("ul.post-list li article[data-tag-" + tag + "]").parent().fadeIn()
+            })
+    	})
     });
 })()
