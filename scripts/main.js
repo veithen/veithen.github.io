d0acfa893301639a5665429078a572ce13da0a8d
Andreas Veithen <andreas.veithen@gmail.com>
2015-01-15 20:32:39 +0000

    Fix JavaScript warnings

--- scripts/main.js
+++ scripts/main.js
@@ -3,45 +3,45 @@
         $("#tabs").tabs();
         $("#tabs .ui-tabs-panel").each(function() {
             var category = $(this).attr("id").substring(5);
-            var tagCloud = $(this).find("ul.tag-box")
-            var postList = $(this).find("ul.post-list")
-            var tags = {}
+            var tagCloud = $(this).find("ul.tag-box");
+            var postList = $(this).find("ul.post-list");
+            var tags = {};
             postList.find("meta[itemprop='keywords']").each(function() {
                 $.each($(this).attr("content").split(","), function(index, tag) {
                     if (tags[tag]) {
-                        tags[tag]++
+                        tags[tag]++;
                     } else {
-                        tags[tag] = 1
+                        tags[tag] = 1;
                     }
-                })
-            })
+                });
+            });
             $(this).bind("tagSelected", function(event, tag) {
                 postList.find("meta[itemprop='keywords']").each(function() {
-                    var li = $(this).parent().parent()
-                    var matches = $(this).attr("content").split(",").indexOf(tag) != -1
+                    var li = $(this).parent().parent();
+                    var matches = $(this).attr("content").split(",").indexOf(tag) != -1;
                     if (matches != $(li).is(":visible")) {
                         if (matches) {
-                            $(li).show("slow")
+                            $(li).show("slow");
                         } else {
-                            $(li).hide("slow")
+                            $(li).hide("slow");
                         }
                     }
-                })
+                });
             });
             $.each(tags, function(tag, count) {
-                var li = $("<li></li>")
-                li.appendTo(tagCloud)
-                var a = $("<a href=''>" + tag + " <span>" + count + "</span></a>")
-                a.appendTo(li)
+                var li = $("<li></li>");
+                li.appendTo(tagCloud);
+                var a = $("<a href=''>" + tag + " <span>" + count + "</span></a>");
+                a.appendTo(li);
                 // TODO: we might use a simple href as well, but we will support multiple selections later
                 a.click(function(e) {
-                    e.preventDefault()
+                    e.preventDefault();
                     window.location = "#" + category + ";" + tag;
-                })
-            })
+                });
+            });
             postList.find("li").hide();
             postList.find("li").removeClass("hidden");
-        })
+        });
         processHash = function() {
             var hash = window.location.hash;
             if (hash && hash.substring(0, 1) == "#") {
@@ -65,4 +65,4 @@
             processHash();
         });
     });
-})()
+})();
