6cd20fd5ffa3604f8a69106539c6675fb6a01344
Andreas Veithen <andreas.veithen@gmail.com>
2015-02-12 19:57:50 +0000

    Configure require.js synchronously to avoid random failures

--- _includes/main.js
+++ _includes/main.js
@@ -1,4 +1,5 @@
 require.config({
+    baseUrl: '/scripts',
     paths: {
         "jquery": "jquery.min",
         "jquery-ui": "jquery-ui.min",
---
+++ scripts/modules.js
@@ -0,0 +1,5 @@
+---
+---
+
+{% include require.js %}
+{% include main.js %}
