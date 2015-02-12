1dec14f0322803b86bc098c21d0fd4697b02ad11
Andreas Veithen <andreas.veithen@gmail.com>
2015-02-12 20:37:01 +0000

    Load Google Analytics using require.js

--- _includes/main.js
+++ _includes/main.js
@@ -6,3 +6,14 @@ require.config({
         "chart": "Chart.min",
     }
 });
+
+window.GoogleAnalyticsObject = "__ga__";
+window.__ga__ = {
+    q: [["create", "UA-56790914-1", "auto"],
+        ["set", "forceSSL", true]],
+    l: Date.now()
+};
+
+define("ga", ["//www.google-analytics.com/analytics.js"], function() {
+    return window.__ga__;
+});
